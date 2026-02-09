629eed6fa1c03dd23e0656df02b3e20a93af830d
carlos griep <carlos.griep@aluno.senai.br>
2026-02-09 11:25:54 -0300

    Add files via upload

---
+++ senai_090226/Program.cs
@@ -0,0 +1,81 @@
+﻿////1////
+// Console.WriteLine("me fale dois numero");
+// int numero =int.Parse(Console.ReadLine());
+// int numeroDois =int.Parse(Console.ReadLine());
+
+// int soma = (numero + numeroDois);
+// int subtrair = (numero - numeroDois);
+// int multiplicar = (numero * numeroDois);
+// Console.WriteLine($"{soma},{subtrair},{multiplicar}");
+
+
+////2////
+// Console.WriteLine("Digitedois numeros");
+// int numero =int.Parse(Console.ReadLine());
+// int numeroDois =int.Parse(Console.ReadLine());
+
+// if (numero > numeroDois)
+// {
+//     Console.WriteLine("o numero 1 é maior que o numero 2");
+// }
+// else if (numero == numeroDois)
+// {
+//     Console.WriteLine("o numero 1 e o 2 são iguais");
+// }
+// else
+// {
+//     Console.WriteLine("o numero 2 é maior que o numero 1");
+
+// }
+
+// Console.WriteLine("Qual sua nota");
+// int nota = int.Parse(Console.ReadLine());
+
+// if (nota > 6)
+// {
+//     Console.WriteLine("Voce foi aprovado");
+// }
+// else if (nota == 5)
+// {
+//     Console.WriteLine("Voce esta de recuperação ");
+// }
+// else
+// {
+//     Console.WriteLine("Voce foi reprovado");
+
+// }
+
+// using System.Threading.Tasks.Sources;
+
+// Console.WriteLine("Qual sua idade");
+// int idade = int.Parse(Console.ReadLine());
+
+// if (idade>50)
+// {
+// Console.WriteLine("voçê é um idoso");
+// }
+// else if (idade<17 )
+// {
+// Console.WriteLine("Voçê é uma criança");
+// }
+// else 
+// {
+// Console.WriteLine("Voçê é um adulto");
+    
+// }
+
+Console.WriteLine("digite um numero positivo ou negativo");
+
+int numero = int.Parse(Console.ReadLine());
+if (numero>0)
+{
+Console.WriteLine("seu numero é positivo");
+}
+else if (numero <0)
+{
+Console.WriteLine("seu numero é negativo");
+}
+else
+{
+Console.WriteLine("seu numero é 0"); 
+}
